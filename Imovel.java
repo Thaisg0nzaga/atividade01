2104a9f047dbea9435ab75ab0c3c7dc878f23e5e
Thaisg0nzaga <141633430+Thaisg0nzaga@users.noreply.github.com>
2024-04-29 08:27:42 -0300

    Create Imovel.java

---
+++ Imovel.java
@@ -0,0 +1,23 @@
+class Imovel {
+    private final String nomeProprietario;
+    private final double imposto;
+    private final int mesesAtraso;
+
+    public Imovel(String nomeProprietario, double imposto, int mesesAtraso) {
+        this.nomeProprietario = nomeProprietario;
+        this.imposto = imposto;
+        this.mesesAtraso = mesesAtraso;
+    }
+
+    public String getNomeProprietario() {
+        return nomeProprietario;
+    }
+
+    public double getImposto() {
+        return imposto;
+    }
+
+    public int getMesesAtraso() {
+        return mesesAtraso;
+    }
+}
